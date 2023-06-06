cadf4f997b6a0a6fa8f6ecc82c7bb1c205f62af0
Patrick Bowen <phunanon@users.noreply.github.com>
2023-06-06 22:20:52 +0100

    Remove ant wandering

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,15 @@
 type Vec = { x: number; y: number };
-type Ant = Vec & {
-  direction: number;
-  home: number;
-  food: number;
-  wandering: boolean;
-  carrying: boolean;
-};
-type Food = Vec & { direction: number; radius: number };
+type Vec3 = Vec & { direction: number };
+type Ant = Vec3 & { home: number; food: number; carrying: boolean };
+type Food = Vec3 & { radius: number };
+type Wall = { a: Vec; b: Vec };
 
 const scale = 6;
 const speed = 1;
 const screamRadius = 16;
 const ants: Ant[] = [];
 const foods: Food[] = [];
+const newHome = { x: 0, y: 0 };
 const home = { x: 0, y: 0, radius: 4 };
 let collected = 0;
 const w = () => document.body.offsetWidth / scale;
@@ -22,14 +19,20 @@ window.addEventListener('DOMContentLoaded', () => {
   const ctx = document.querySelector('canvas')?.getContext('2d');
   if (!ctx) return;
   ctx.canvas.addEventListener('mousemove', e => {
-    home.x = e.offsetX / scale;
-    home.y = e.offsetY / scale;
+    newHome.x = e.offsetX / scale;
+    newHome.y = e.offsetY / scale;
+  });
+  document.body.addEventListener('keyup', e => {
+    if (e.key === ' ') {
+      home.x = newHome.x;
+      home.y = newHome.y;
+    }
   });
 
   ants.push(
     ...new Array(512).fill(0).map(() => ({
       ...{ x: Math.random() * w(), y: Math.random() * h() },
-      ...{ home: 0, food: 0, wandering: true, carrying: false },
+      ...{ home: 0, food: 0, carrying: false },
       direction: Math.random() * Math.PI * 2,
     }))
   );
@@ -101,53 +104,52 @@ const near = (a: Vec, b: Vec, radius: number) =>
   a.y > b.y - radius &&
   a.y < b.y + radius;
 
+/** Moves an object and bounces it off the canvas bounds */
+const move = (vec: Vec3, radius = 0, speedMult = 1) => {
+  vec.x += Math.cos(vec.direction) * speed * speedMult;
+  vec.y += Math.sin(vec.direction) * speed * speedMult;
+
+  if (vec.x < radius) {
+    vec.x = radius;
+    vec.direction = Math.PI - vec.direction;
+  }
+  if (vec.x + radius > w()) {
+    vec.x = w() - radius;
+    vec.direction = Math.PI - vec.direction;
+  }
+  if (vec.y < radius) {
+    vec.y = radius;
+    vec.direction = -vec.direction;
+  }
+  if (vec.y + radius > h()) {
+    vec.y = h() - radius;
+    vec.direction = -vec.direction;
+  }
+};
+
 const Update = () => {
   const updates: [number, number, number, number, boolean, boolean][] = [];
 
-  foods.forEach(food => {
-    food.x += Math.cos(food.direction) * speed * 0.2;
-    food.y += Math.sin(food.direction) * speed * 0.2;
-
-    if (food.x < food.radius) {
-      food.direction = Math.PI - food.direction;
-      food.x = food.radius;
-    }
-    if (food.x + food.radius > w()) {
-      food.direction = Math.PI - food.direction;
-      food.x = w() - food.radius;
-    }
-    if (food.y < food.radius) {
-      food.direction = -food.direction;
-      food.y = food.radius;
-    }
-    if (food.y + food.radius > h()) {
-      food.direction = -food.direction;
-    }
-  });
+  foods.forEach(food => move(food, food.radius, 0.2));
 
   ants.forEach(ant => {
-    ant.x += Math.cos(ant.direction) * speed;
-    ant.y += Math.sin(ant.direction) * speed;
+    move(ant);
 
     if (ant.x < 0) {
       ant.direction = Math.PI - ant.direction;
       ant.x = 0;
-      ant.wandering = true;
     }
     if (ant.x > w()) {
       ant.direction = Math.PI - ant.direction;
       ant.x = w();
-      ant.wandering = true;
     }
     if (ant.y < 0) {
       ant.direction = -ant.direction;
       ant.y = 0;
-      ant.wandering = true;
     }
     if (ant.y > h()) {
       ant.direction = -ant.direction;
       ant.y = h();
-      ant.wandering = true;
     }
 
     if (near(ant, home, home.radius) && ant.carrying) {
@@ -174,9 +176,6 @@ const Update = () => {
       });
     }
 
-    if (ant.wandering) {
-      ant.direction += Math.random() * 0.1 - 0.05;
-    }
     ++ant.food;
     ++ant.home;
   });
@@ -205,7 +204,6 @@ const Update = () => {
       }
       if (goFood || goHome) {
         other.direction = newDir;
-        other.wandering = false;
       }
     });
   });
